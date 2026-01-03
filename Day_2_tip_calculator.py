5f0407e7d1fb1017620b5b8cbb39c07c77bcd198
sathishkumar-lab <vsathishsk183@gmail.com>
2026-01-03 22:41:06 +0530

    Add tip calculator program

---
+++ Day_2_tip_calculator.py
@@ -0,0 +1,6 @@
+print("\n\t\t\t\033[1m Welcome to tip calculator! \033[0m")
+bill=int(input("\n Enter the total bill : "))
+tip=int(input("\n How much tip would you like to give? 5, 7, 10 : "))
+member=int(input("\n How many member do you want to split : "))
+total_bill=(bill+(bill*tip/100))/member
+print("\n Total amount each should pay is : ", total_bill)
