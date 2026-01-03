9b47170898faa03db18a48b58b14de20757fdd6b
sathishkumar-lab <vsathishsk183@gmail.com>
2026-01-03 22:30:36 +0530

    Brand Name Generator script

---
+++ Day_1_Band Name Generator.py
@@ -0,0 +1,4 @@
+print("\t\t\t\033[1mWelcome to Brand Name Generator\033[0m")
+city=input("\nEnter Your City Name : ")
+business=input("\nEnter your business Name: ")
+print("\nYour Brand Name could be : " + city + " " +business)
