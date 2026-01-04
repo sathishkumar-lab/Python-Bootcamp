eef5f5b7604bb54a55c77bdd4a01541a9a97f984
sathishkumar-lab <vsathishsk183@gmail.com>
2026-01-04 23:02:10 +0530

    Rock, Paper, Scissors game

---
+++ Day_4.py
@@ -0,0 +1,50 @@
+import random
+rock="""
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper="""
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors="""
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+game_images=[rock,paper,scissors]
+
+user_choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors : "))
+
+if user_choice>=0 and user_choice<=2:
+    print(game_images[user_choice])
+
+computer_choice=random.randint(0,2)
+print("Computer Choose : ")
+print(game_images[computer_choice])
+
+if user_choice>=3 or user_choice<0:
+    print("You typed an invalid number. You loss.")
+elif user_choice==computer_choice:
+    print("Match Draw")
+elif user_choice==0 and computer_choice==2:
+    print("You Win!")
+elif computer_choice==0 and user_choice==2:
+    print("You Loss!")
+elif  user_choice > computer_choice :
+    print("You Win!")
+elif  computer_choice > user_choice :
+    print("You Loss!")
