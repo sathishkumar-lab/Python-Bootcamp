04b1a8e47d85c9f4a2cc11ee4435655764f209e1
sathishkumar-lab <vsathishsk183@gmail.com>
2026-01-06 22:07:11 +0530

    Brand Name Generator

--- Day_1.py
+++ Day_1.py
@@ -1,4 +1,4 @@
-print("\n\t\t\t\033[1mWelcome to Brand Name Generator\033[0m")
+print("\t\t\t\033[1mWelcome to Brand Name Generator\033[0m")
 city=input("\nEnter Your City Name : ")
 business=input("\nEnter your business Name: ")
 print("\nYour Brand Name could be : " + city + " " +business)
